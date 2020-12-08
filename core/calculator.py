e4655fafabcaf0cd073b3cc4ac7541d42ea67c30
MohmmadReza-Jafari <chashm78@gmail.com>
2020-12-09 01:03:39 +0330

    write Fibonacci function

--- core/calculator.py
+++ core/calculator.py
@@ -40,3 +40,12 @@ def timeit(func, currently_evaluating=None):
             return value
     return wrapper
 
+@timeit()
+def fibonacci(n=0):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibonacci(n-1) + fibonacci(n-2)
+
+
