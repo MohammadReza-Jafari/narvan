379115b2750715bd87eb016c573148380a85a5d0
MohmmadReza-Jafari <chashm78@gmail.com>
2020-12-08 19:02:35 +0330

    config admin site for Report modle

--- core/admin.py
+++ core/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from core.models import Report
 
-admin.site.register(Report)
+
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ('created_time', 'func_name', 'inputs', 'time_spent')
+    list_filter = ('created_time',)
+    search_fields = ('func_name',)
+
+
+admin.site.register(Report, ReportAdmin)
