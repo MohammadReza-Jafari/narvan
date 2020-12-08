9cb66a7c2fccf2a6c79e7e94600d6ce0de83debd
MohmmadReza-Jafari <chashm78@gmail.com>
2020-12-09 01:22:00 +0330

    change FloatField to DecimalField for time spent

--- core/models.py
+++ core/models.py
@@ -5,7 +5,7 @@ class Report(models.Model):
     created_time = models.DateTimeField(auto_now_add=True)
     func_name = models.CharField(max_length=30)
     inputs = models.CharField(max_length=20)
-    time_spent = models.FloatField()
+    time_spent = models.DecimalField(verbose_name="Time Spent (ms)", max_digits=8, decimal_places=4)
 
     def __str__(self):
         return self.func_name
